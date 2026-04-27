5afc1742acd3155d193a0b85a9bc3cb5fed69098
Dmitry Zamyatin <dizamer@yandex.ru>
2026-04-27 04:59:56 +0000

    Правка сборки контейнера, gitignore файлов сборки

--- ros2_ws/build/comp_vision_interfaces/ament_cmake_python/comp_vision_interfaces/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='comp_vision_interfaces',
-    version='0.0.0',
-    packages=find_packages(
-        include=('comp_vision_interfaces', 'comp_vision_interfaces.*')),
-)
--- ros2_ws/build/interfaces/rosidl_generator_py/interfaces/srv/__init__.py
+++
@@ -1,3 +0,0 @@
-from interfaces.srv._get_fen import GetFEN  # noqa: F401
-from interfaces.srv._get_move import GetMove  # noqa: F401
-from interfaces.srv._move import Move  # noqa: F401
